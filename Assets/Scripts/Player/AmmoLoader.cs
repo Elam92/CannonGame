62eccd381eeb297557abcde9300d3554141bfc6e
Eric <elam3@ualberta.ca>
2018-06-04 09:00:38 -0600

    Forgot to comment a script.

--- Assets/Scripts/Player/AmmoLoader.cs
+++ Assets/Scripts/Player/AmmoLoader.cs
@@ -2,11 +2,19 @@
 
 namespace CannonGame {
 
+    /*  AmmoLoader Class
+     * 
+     *  Contains the different types of projectiles the Player can use.
+     */
     public class AmmoLoader : MonoBehaviour, IPoolUser {
 
+        // The different projectiles.
         public Projectile[] projectileTypes;
 
+        // Pooler Manager reference to reuse the projectiles.
         private IPoolManager pooler;
+
+        // The current projectile being used.
         private Projectile currentType;
 
         private void Start()
@@ -14,6 +22,7 @@ namespace CannonGame {
 
         }
 
+        // Instantiate a projectile to be launched.
         public Projectile UseType(Vector3 position, Quaternion rotation)
         {
             if (projectileTypes.Length == 0)
@@ -24,6 +33,7 @@ namespace CannonGame {
             return pooler.UseObject(currentType, position, rotation) as Projectile;
         }
 
+        // Set projectile type by index.
         public void SetType(int index)
         {
             if (index < 0)
@@ -38,11 +48,13 @@ namespace CannonGame {
             currentType = projectileTypes[index];
         }
 
+        // Look at the next projectile of current type.
         public Projectile GetCurrentType()
         {
             return pooler.Peek(currentType) as Projectile;
         }
 
+        // Create pools of the projectiles that we have in our container.
         public void InitializeWithPooler(IPoolManager pooler, int poolSize)
         {
             this.pooler = pooler;
